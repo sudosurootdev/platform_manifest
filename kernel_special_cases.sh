835cee945b209ef447c49e5fc52ba83596642e4b
nuclearmistake <nuclearmistake@gmail.com>
2017-05-19 06:42:12 -0400

    Our libra hasnt been updated in months. try cm's new trees
    Change-Id: If6ae28af6c7bb10a3056d0aeb3950e5aef1fbc54

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -26,11 +26,6 @@ case $1 in
         export remoterevision="n71"
         export kernelsource="kernel_lge_msm8974"
     ;;
-    libra)
-        export remote="vanir"
-        export remoterevision="n71"
-        export kernelsource="kernel_xiaomi_msm8992"
-    ;;
     mako)
         export kernelsource="lge-kernel-mako"
     ;;
